7bd5a35c7a52003aa2267175528c23b4cdc479e0
mt5225 <mt5225@gmail.com>
2014-12-31 13:13:34 +0800

    trim()

--- lab/searchSTP.js
+++ lab/searchSTP.js
@@ -6,8 +6,8 @@ function getproducts() {
     var products = document.querySelectorAll('.productTitle a');
     return Array.prototype.map.call(products, function(e) {
         return {
-            "url" : e.getAttribute('href'),
-            "title" : e.textContent
+            "url" : e.getAttribute('href').trim(),
+            "title" : e.textContent.trim();
         }});
 }
 
@@ -24,7 +24,7 @@ casper.run(function() {
     // echo results in some pretty fashion
     this.echo(products.length + ' products found for patagonia:');
     for (var i = products.length - 1; i >= 0; i--) {
-        this.echo(' - ' + JSON.stringify(products[i]) + "\n");
+        this.echo(' - ' + JSON.stringify(products[i]));
     };
 
     this.exit();
